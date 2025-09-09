f59e65752db657eb6537ba748821f6a7373763fd
Alvaro Bautista <alvarobp@gmail.com>
2025-09-09 14:12:53 +0200

    Allow deploying daily digest as self-contained docker image

--- projects/daily-digest/generate.sh
+++ projects/daily-digest/generate.sh
@@ -2,6 +2,8 @@
 
 source .env
 
+echo "Generating digest..."
 cat project-instructions.md additions-for-unattended.md | \
   claude -p && \
+  echo "Uploading digest..." && \
   aws s3 sync --acl public-read digests/ s3://alvarobp-digests/daily-digests/
